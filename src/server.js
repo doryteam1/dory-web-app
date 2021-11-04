e4cfaf23727e287f6320aa97218c3695aeaa33bb
Renzo Funes Meza <rfunez@gmail.com>
2021-11-04 05:38:14 -0500

    server.js ajuste

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/../dist/web-app-dory'));
+app.use(express.static(__dirname + '/dist/web-app-dory'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../dist/web-app-dory/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/web-app-dory/index.html'));
 });
 
 // default Heroku port
