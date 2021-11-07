677043789e7a453f88f137827eb2bf3b0d1e36f8
Renzo Funes Meza <rfunez@gmail.com>
2021-11-07 12:04:46 -0500

    mapa svg sucre agregado

--- src/app/granjas/components/granjas/granjas.component.ts
+++ src/app/granjas/components/granjas/granjas.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./granjas.component.scss']
 })
 export class GranjasComponent implements OnInit {
-  
+  sanOnofreActive:boolean = true;
+  toluActive:boolean = true;
+  toluViejoActive:boolean = true;
+  covenasActive:boolean = true;
+  colosoActive:boolean = true;
+  chalanActive:boolean = true;
+  ovejasActive:boolean = true;
+  palmitoActive:boolean = true;
+  sincelejoActive:boolean = true;
+  morroaActive:boolean = true;
+  losPalmitosActive:boolean = true;
+  sanPedroActive:boolean = true;
+  sampuesActive:boolean = true;
+  corozalActive:boolean = true;
+  sanJuanDeBetuliaActive:boolean = true;
+  sinceActive:boolean = true;
+  buenaVistaActive:boolean = true;
+  elRobleActive:boolean = true;
+  galerasActive:boolean = true;
+  sanBenitoAbadActive:boolean = true;
+  laUnionActive:boolean = true;
+  caimitoActive:boolean = true;
+  sucreActive:boolean = true;
+  sanMarcosActive:boolean = true;
+  majagualActive:boolean = true;
+  guarandaActive:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
