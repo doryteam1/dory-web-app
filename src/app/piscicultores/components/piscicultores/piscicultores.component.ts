87022d9f25a8edfdc3c1b2478dce27cab3b291dd
Renzo Funes Meza <rfunez@gmail.com>
2021-11-19 06:19:15 -0500

    ajuste menu productores

---
+++ src/app/piscicultores/components/piscicultores/piscicultores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piscicultores',
+  templateUrl: './piscicultores.component.html',
+  styleUrls: ['./piscicultores.component.scss']
+})
+export class PiscicultoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/piscicultores/piscicultores/piscicultores.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PiscicultoresRoutingModule } from './piscicultores-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PiscicultoresRoutingModule
+  ]
+})
+export class PiscicultoresModule { }
