4cec1f7bd39609c35c53e2707737151b968e3635
Renzo Funes Meza <rfunez@gmail.com>
2022-03-08 13:12:49 -0500

    correccion modal y icon like cambio color

--- src/app/modals/modal-options/modal-options.component.ts
+++ src/app/modals/modal-options/modal-options.component.ts
@@ -5,18 +5,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   selector: 'ngbd-modal-options',
   templateUrl: './modal-options.component.html',
   encapsulation: ViewEncapsulation.None,
-  styles: [`
-    .dark-modal .modal-content {
-      background-color: #292b2c;
-      color: white;
-    }
-    .dark-modal .close {
-      color: white;
-    }
-    .light-blue-backdrop {
-      background-color: #5cb3fd;
-    }
-  `]
+  styleUrls: ['./modal-options.component.scss']
 })
 export class NgbdModalOptions {
   closeResult: string = '';
