23a70bda42b795f9505d72df765c0386453e1b98
Renzo Funes Meza <rfunez@gmail.com>
2022-04-29 11:36:24 -0500

    polilineas mapa de sucre - boundaries restrictions - marker restriction

--- src/app/shared/components/floating-btn-auto-up/floating-btn-auto-up.component.ts
+++ src/app/shared/components/floating-btn-auto-up/floating-btn-auto-up.component.ts
@@ -1,24 +1,42 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-floating-btn-auto-up',
   templateUrl: './floating-btn-auto-up.component.html',
-  styleUrls: ['./floating-btn-auto-up.component.scss']
+  styleUrls: ['./floating-btn-auto-up.component.scss'],
 })
 export class FloatingBtnAutoUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  windowScrolled: boolean | undefined;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
   }
-
-  onClick(){
-
-  }
-
   scrollToBottom(): void {
-    console.log("Scroll");
-    //window.scrollTo(0,document.body.scrollHeight);
-    window.scrollTo(0,0);
-}
+    (function smoothscroll() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    })();
+  }
+  ngOnInit(): void {}
+
+  onClick() {}
+  /*  scrollToBottom(): void {
+    console.log('Scroll');
+    window.scrollTo(0, 0);
+  } */
 }
