502d51119d98fc1d134d668e0c02b62fc6eb1321
Andres Davila Arroyo <davilaarroyo07@gmail.com>
2022-05-09 16:15:38 -0500

    mejora del mapa

---
+++ src/app/shared/components/confirm-modal-map/confirm-modal-map.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-confirm-modal-map',
+  templateUrl: './confirm-modal-map.component.html',
+  styleUrls: ['./confirm-modal-map.component.scss']
+})
+export class ConfirmModalMapComponent implements OnInit {
+  @Input() iconTicle:string=""
+  @Input() icon:string=""
+  @Input() title: string = 'Confirmar acción';
+  @Input() message: string = '¿Esta seguro de completar esta acción?';
+  @Input() btnOkText: string = 'Aceptar';
+  @Input() btnCancelText: string = 'Cancelar';
+  @Input() resourceId: string = '';
+
+  constructor(private _modalService: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  public decline(){
+    this._modalService.close(false)
+  }
+
+  public accept(){
+    this._modalService.close(true)
+  }
+
+  public dismiss(){
+    this._modalService.dismiss()
+  }
+}
+
