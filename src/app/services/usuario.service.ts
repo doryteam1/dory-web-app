b282e959444658e9d7bee414fa494ba43b8817e7
Renzo Funes Meza <rfunez@gmail.com>
2022-07-04 17:46:41 -0500

    ajuste registro usuario

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -12,23 +12,12 @@ export class UsuarioService {
 
   registrarUsuario(usuario:any):Observable<any>{
     let body = {
-        cedula : usuario.cedula || '',
         nombres : usuario.nombres || '', 
         apellidos : usuario.apellidos || '',
-        celular: usuario.celular || '',
-        direccion: usuario.direccion || '', 
         id_tipo_usuario : usuario.tipoUsuario || '',
         email: usuario.email || '',
         password: usuario.password || '', 
-        id_area_experticia:usuario.id_area_experticia || '',
-        nombre_negocio:usuario.nombre_negocio || '',
         foto:usuario.foto || '', 
-        fecha_registro:usuario.fecha_registro || '',
-        fecha_nacimiento:usuario.fechaNac || '',
-        id_departamento: usuario.departamento || '',
-        id_municipio:usuario.municipio || '',
-        id_corregimiento:usuario.corregimiento || '',
-        id_vereda:usuario.vereda || '',
         latitud:usuario.latitud || 0,
         longitud:usuario.longitud || 0,
         creadoCon:usuario.creadoCon || ''
