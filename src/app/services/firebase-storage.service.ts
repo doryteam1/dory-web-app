9c53fd5019f153410029f7d7f3e7ec14cfbf445c
Andres Davila Arroyo <davilaarroyo07@gmail.com>
2022-07-09 08:48:09 -0500

    mejora perfil

--- src/app/services/firebase-storage.service.ts
+++ src/app/services/firebase-storage.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  doc,
+  docData,
+  deleteDoc,
+  updateDoc,
+  DocumentReference,
+  setDoc,
+} from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseStorageService {
+  constructor(private storage: AngularFireStorage) {}
 
-  constructor(private storage:AngularFireStorage) { }
+  public cloudStorageTask(fileName: string, data: any) {
+    return this.storage.upload(fileName, data);
+  }
 
-    public cloudStorageTask(fileName: string, data: any) {
-      return this.storage.upload(fileName, data);
-    }
-  
-    public cloudStorageRef(fileName: string) {
-      return this.storage.ref(fileName);
-    }
+  public cloudStorageRef(fileName: string) {
+    return this.storage.ref(fileName);
+  }
+  public deletephotoPerfil(fileName: string) {
+    return this.storage.ref(fileName).delete();
+  }
 
-    public refFromUrl(path:string){
-      return this.storage.refFromURL(path);
-    }
+  public refFromUrl(path: string) {
+    console.log(this.storage.refFromURL(path));
+    return this.storage.refFromURL(path);
+  }
 }
