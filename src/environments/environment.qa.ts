bb9e040e7ddacbb7ac92991d1208f373c374bca3
Renzo Funes Meza <rfunez@gmail.com>
2022-08-29 11:33:01 -0500

    configuracion temporal

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,20 @@
 export const environment = {
     firebase: {
+      projectId: 'dory-qa-83409',
+      appId: '1:170816600260:web:3ecb1b742c41763559f191',
+      storageBucket: 'dory-qa-83409.appspot.com',
+      locationId: 'us-central',
+      apiKey: 'AIzaSyBoYqkThi_lV_YldjGTEuNyODt4OjBG4Z4',
+      authDomain: 'dory-qa-83409.firebaseapp.com',
+      messagingSenderId: '170816600260',
+    },
+    doryApiKey:'AIzaSyDxAJesdH6yoUCT79wtRqXYKSevJsPD0TU',
+    production: false,
+    doryApiRestBaseUrl:'https://dory-api-rest-pruebas.herokuapp.com//api'
+  };
+  
+  /* export const environment = {
+    firebase: {
       projectId: process.env.FIREBASE_PROJECT_ID,
       appId: process.env.FIREBASE_APP_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -12,4 +27,4 @@ export const environment = {
     production: false,
     doryApiRestBaseUrl:'https://dory-api-rest-pruebas.herokuapp.com/api'
   };
-  
+   */
