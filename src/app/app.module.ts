6e6cdb49015314e5d1fd1c51df0ca278b5f6050e
Renzo Funes Meza <rfunez@gmail.com>
2022-08-30 06:45:05 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { ShareButtonsConfig } from 'ngx-sharebuttons';
 import { TransportadorDetalleComponent } from './transportadores/components/transportador-detalle/transportador-detalle.component';
 import { VehiculoDetalleComponent } from './vehiculos/components/vehiculo-detalle/vehiculo-detalle.component';
+import { NgxEnvModule } from '@ngx-env/core';
 
 const customConfig: ShareButtonsConfig = {
   include: ['whatsapp', 'email', 'copy'],
@@ -68,6 +69,7 @@ const customConfig: ShareButtonsConfig = {
     ShareIconsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireStorageModule,
+    NgxEnvModule
   ],
   providers: [
     HttpsService,
