7fdafb9cc593f5e684d6dfb6b9703e07c930c274
Renzo Funes Meza <rfunez@gmail.com>
2022-08-31 11:24:28 -0500

    test

--- webpack.partial.js
+++ webpack.partial.js
@@ -4,15 +4,15 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-            "process.env.FIREBASE_APP_ID": process.env.FIREBASE_APP_ID,
-            "process.env.FIREBASE_STORAGE_BUCKET": process.env.FIREBASE_STORAGE_BUCKET,
-            "process.env.FIREBASE_LOCATION_ID": process.env.FIREBASE_LOCATION_ID,
-            "process.env.FIREBASE_API_KEY": process.env.FIREBASE_API_KEY,
-            "process.env.FIREBASE_AUTH_DOMAIN": process.env.FIREBASE_AUTH_DOMAIN,
-            "process.env.FIREBASE_MESSAGING_SENDER_ID": process.env.FIREBASE_MESSAGING_SENDER_ID,
-            "process.env.MAPS_API_KEY": process.env.MAPS_API_KEY,
+            "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
+            "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+            "process.env.FIREBASE_LOCATION_ID": JSON.stringify(process.env.FIREBASE_LOCATION_ID),
+            "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
+            "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+            "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            "process.env.MAPS_API_KEY": JSON.stringify(process.env.MAPS_API_KEY),
             "process.env.DORY_API_REST": JSON.stringify(process.env.DORY_API_REST),
-            "process.env.OAUTH_CLIENT_ID": process.env.OAUTH_CLIENT_ID,
+            "process.env.OAUTH_CLIENT_ID": JSON.stringify(process.env.OAUTH_CLIENT_ID),
             "VERSION": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN)
         })
     ]
