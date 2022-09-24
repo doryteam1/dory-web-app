92f718e72ed4613e13f00c8d388f9589374cb485
Andres Davila Arroyo <davilaarroyo07@gmail.com>
2022-09-23 19:29:45 -0500

    agregando componente app-no-fout

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -3,14 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
-  @Input() objectName:string = 'objetos'
-  @Input() message:string = ''
-  constructor() { }
+  @Input() objectName: string = 'objetos';
+  @Input() message: string = '';
+  @Input() messageError: string = '';
+  @Input() errorData: boolean = false;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
