bfd1ef75c9e72fff6a72f3af5b2305d7e8129879
Renzo Funes Meza <rfunez@gmail.com>
2022-12-29 16:47:07 -0500

    avances manual

--- src/app/manual/components/docs/docs.component.ts
+++ src/app/manual/components/docs/docs.component.ts
@@ -1,13 +1,14 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+declare var window: any;
 @Component({
   selector: 'app-docs',
   templateUrl: './docs.component.html',
   styleUrls: ['./docs.component.scss']
 })
 export class DocsComponent implements OnInit {
-  innerWidth:number = 1244;
+  innerWidth:number = window.innerWidth;
+  offcanvas: any;
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.innerWidth = window.innerWidth;
@@ -16,6 +17,18 @@ export class DocsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.offcanvas = new window.bootstrap.Offcanvas(
+      document.getElementById('offcanvas'),
+      {
+        backdrop: true,
+      }
+    );
   }
 
+  openOffcanvas() {
+    this.offcanvas.show();
+  }
+  closeOffcanvas() {
+    this.offcanvas.hide();
+  }
 }
