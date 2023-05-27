acb08c2d92de2e14d726a2efb7b346851c83deb9
Renzo Funes Meza <rfunez@gmail.com>
2023-05-26 19:17:47 -0500

    Middleware para redirigir HTTP a HTTPS

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Middleware para redirigir HTTP a HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(`https://${req.hostname}${req.originalUrl}`);
+  } else {
+    next();
+  }
+});
+
 // Serve static files
 app.use(express.static(__dirname + '/dist/web-app-dory'));
 
